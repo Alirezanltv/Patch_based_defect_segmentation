93a5bf94fb634c7b655e684f7e806dc35590fe51
Ali <40850908+Alirezanltv@users.noreply.github.com>
2021-11-06 19:48:56 +0330

    Add files via upload

---
+++ read_image.py
@@ -0,0 +1,25 @@
+def read_images(path_directory):
+
+  image_file_path = path_directory
+  image_file = glob.glob(image_file_path +'*.PNG')
+  image_file.sort()
+  X= []
+  for img in image_file:
+      image = cv2.imread(img)
+      image = cv2.resize(image ,(448,448))
+      # image equalization with clahe algorithm
+      lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+      lab_planes = cv2.split(lab)
+      clahe = cv2.createCLAHE(clipLimit=2,tileGridSize=(5,5))
+      lab_planes[0] = clahe.apply(lab_planes[0])
+      lab = cv2.merge(lab_planes)
+      image = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+      # ////end of clahe process
+      image = image.astype('float32')
+      image = image /255.
+      # image = rgb2gray(image)
+      image = image.reshape(448,448,3)
+      X.append(image)
+      
+  X= np.array(X)
+  return X
